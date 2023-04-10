f4b61537df552325a1434fa276643146d6090655
nipaaa <akternipa941@gmail.com>
2023-04-10 14:41:23 +0600

    update

--- src/components/Home/Event.js
+++ src/components/Home/Event.js
@@ -5,85 +5,138 @@ import blueprint from "../../assets/home/blueprint.png";
 import location from "../../assets/logo/location.png";
 import clock from "../../assets/logo/clock.png";
 import calender from "../../assets/logo/calender.png";
-import GradientBtn from "../../utils/GradientBtn";
 
 const Event = () => {
   return (
-    <div className="container">
-      <Title>
-        <span>Join Event</span>
-      </Title>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-[30px]">
-        <div className="rounded-[5px]">
-          <img className="w-full" src={mobile} alt="" />
+    <div className="event">
+      <div className="container">
+        <Title>
+          <span>Join Event</span>
+        </Title>
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-[30px]">
+          <div className="rounded-[5px]">
+            <img className="w-full" src={mobile} alt="" />
 
-          <div className="bg-[#EEEEEE] p-[16px] rounded-b-[5px] flex flex-col gap-y-8">
-            <div className="flex justify-between">
-              <div className="flex items-center gap-3">
-                <img className="me-1" src={location} alt="" />
-                <p className="text-[#5F6765] text-[14px]"> Dhaka</p>
+            <div className="bg-[#EEEEEE] p-[16px] rounded-b-[5px] flex flex-col gap-y-8">
+              <div className="flex justify-between">
+                <div className="flex items-center gap-3">
+                  <img className="me-1" src={location} alt="" />
+                  <p className="text-[#5F6765] text-[14px]"> Dhaka</p>
+                </div>
+                <div className="flex items-center gap-3">
+                  <img src={clock} alt="" />
+                  <p className="text-[#5F6765] text-[14px] font-[400]">
+                    12.00 PM-03.00 AM
+                  </p>
+                </div>
               </div>
-              <div className="flex items-center gap-3">
-                <img src={clock} alt="" />
-                <p className="text-[#5F6765] text-[14px] font-[400]">
-                  12.00 PM-03.00 AM
-                </p>
-              </div>
-            </div>
-            <p className="text-[#31374C] text-[16px] font-[600]">
-              Web Development and Mobile app Development
-            </p>
-            <hr />
-            <div className="flex justify-between">
-              <div className="flex items-center gap-3">
-                <img className="me-1" src={calender} alt="" />
-                <p className="text-[#5F6765] text-[14px] font-[400]">
-                  {" "}
-                  20 June 2022
-                </p>
-              </div>
-              <div className="flex items-center gap-3">
-                {/* <GradientBtn>Join Event</GradientBtn> */}
-                <button className="bg-gradient-to-r from-[#FF047D] to-[#FF8804] rounded-[10px] px-[15px] py-[10px] text-white text-[14px] font-[500]">
-                  Join Event
-                </button>
+              <p className="text-[#31374C] text-[16px] font-[600]">
+                Web Development and Mobile app Development
+              </p>
+              <hr />
+              <div className="flex justify-between">
+                <div className="flex items-center gap-3">
+                  <img className="me-1" src={calender} alt="" />
+                  <p className="text-[#5F6765] text-[14px] font-[400]">
+                    {" "}
+                    20 June 2022
+                  </p>
+                </div>
+                <div className="flex items-center gap-3">
+                  <button className="bg-gradient-to-r from-[#FF047D] to-[#FF8804] rounded-[10px] px-[15px] py-[10px] text-white text-[14px] font-[500]">
+                    Join Event
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="rounded-[5px]">
-          <img className="w-full" src={blueprint} alt="" />
+          <div className="rounded-[5px]">
+            <img className="w-full" src={blueprint} alt="" />
 
-          <div className="bg-[#EEEEEE] p-[16px] rounded-b-[5px] flex flex-col gap-y-8">
-            <div className="flex justify-between">
-              <div className="flex items-center gap-3">
-                <img className="me-1" src={location} alt="" />
-                <p className="text-[#5F6765] text-[14px]"> Dhaka</p>
+            <div className="bg-[#EEEEEE] p-[16px] rounded-b-[5px] flex flex-col gap-y-8">
+              <div className="flex justify-between">
+                <div className="flex items-center gap-3">
+                  <img className="me-1" src={location} alt="" />
+                  <p className="text-[#5F6765] text-[14px]"> Dhaka</p>
+                </div>
+                <div className="flex items-center gap-3">
+                  <img src={clock} alt="" />
+                  <p className="text-[#5F6765] text-[14px] font-[400]">
+                    12.00 PM-03.00 AM
+                  </p>
+                </div>
               </div>
-              <div className="flex items-center gap-3">
-                <img src={clock} alt="" />
-                <p className="text-[#5F6765] text-[14px] font-[400]">
-                  12.00 PM-03.00 AM
+              <p className="text-[#31374C] text-[16px] font-[600]">
+                A Career in UI UX Skills, Tools, and a Blueprint to Pivot Into
+                the Field
+              </p>
+              <hr />
+              <div className="flex justify-between">
+                <div className="flex items-center gap-3">
+                  <img className="me-1" src={calender} alt="" />
+                  <p className="text-[#5F6765] text-[14px] font-[400]">
+                    {" "}
+                    20 June 2022
+                  </p>
+                </div>
+                <div className="flex items-center gap-3">
+                  {/* <GradientBtn>Join Event</GradientBtn> */}
+                  <button className="bg-gradient-to-r from-[#FF047D] to-[#FF8804] rounded-[10px] px-[15px] py-[10px] text-white text-[14px] font-[500]">
+                    Join Event
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div className="grid grid-cols-3 gap-[15px] mb-[15px] ">
+              <div className="date h-[116px] flex flex-col items-center justify-center">
+                <p>20</p>
+                <p>June</p>
+              </div>
+              <div className="col-span-2">
+                <p className="title">Graphics Design</p>
+                <p className="content">
+                  A Career in UI/UX Skills, Tools, and a Blueprint to Pivot Into
+                  the Field
                 </p>
               </div>
             </div>
-            <p className="text-[#31374C] text-[16px] font-[600]">
-            A Career in UI UX Skills, Tools, and a Blueprint to Pivot Into the Field
-            </p>
-            <hr />
-            <div className="flex justify-between">
-              <div className="flex items-center gap-3">
-                <img className="me-1" src={calender} alt="" />
-                <p className="text-[#5F6765] text-[14px] font-[400]">
-                  {" "}
-                  20 June 2022
+            <div className="grid grid-cols-3 gap-[15px] mb-[15px]">
+              <div className="date h-[116px] flex flex-col items-center justify-center">
+                <p>20</p>
+                <p>June</p>
+              </div>
+              <div className="col-span-2">
+                <p className="title">Web Development</p>
+                <p className="content">
+                  Web Development and Mobile app Development
                 </p>
               </div>
-              <div className="flex items-center gap-3">
-                {/* <GradientBtn>Join Event</GradientBtn> */}
-                <button className="bg-gradient-to-r from-[#FF047D] to-[#FF8804] rounded-[10px] px-[15px] py-[10px] text-white text-[14px] font-[500]">
-                  Join Event
-                </button>
+            </div>
+            <div className="grid grid-cols-3 gap-[15px] mb-[15px]">
+              <div className="date h-[116px] flex flex-col items-center justify-center">
+                <p>20</p>
+                <p>June</p>
+              </div>
+              <div className="col-span-2">
+                <p className="title">Digital Marketing</p>
+                <p className="content">
+                  Web Development and Mobile app Development
+                </p>
+              </div>
+            </div>
+            <div className="grid grid-cols-3 gap-[15px] mb-[15px]">
+              <div className="date h-[116px] flex flex-col items-center justify-center">
+                <p>20</p>
+                <p>June</p>
+              </div>
+              <div className="col-span-2">
+                <p className="title">Motion Graphics</p>
+                <p className="content">
+                  Web Development and Mobile app Development
+                </p>
               </div>
             </div>
           </div>
