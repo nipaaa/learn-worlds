ecf164ef50c9705e6cef191e4b721e44e9d3747e
MamtazDev <akter.mstmamtaz@gmail.com>
2023-04-15 11:22:14 +0600

    course banner add

--- src/components/Courses/CoursePage.js
+++ src/components/Courses/CoursePage.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import NewsLetters from '../Home/NewsLetters';
+import React from "react";
+import NewsLetters from "../Home/NewsLetters";
+import SectionBanner from "../../utils/SectionBanner";
+import banner from "../../assets/course/course-banner.png";
 
 const CoursePage = () => {
-    return (
-        <div>
-           <NewsLetters/> 
-        </div>
-    );
+  return (
+    <div>
+      <SectionBanner
+        bg="bg-[#F3718B]"
+        route="Courses"
+        content="At ipsum est viverra varius lectus et tempus arcu massa egetpretium at tellus tristique Amet euismod vel suspendisse nunc, ut ru egetpretium trum arcu egetpretium morbi"
+        bannerImg={banner}
+      />
+      <NewsLetters />
+    </div>
+  );
 };
 
 export default CoursePage;
