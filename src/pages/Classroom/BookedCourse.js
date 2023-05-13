5eaa59556c36d8fdb63de8e66aa29acc8784a14f
mohi14 <mohiuddinkhan1409@gmail.com>
2023-05-13 18:50:49 +0600

    some error fixed

--- src/pages/Classroom/BookedCourse.js
+++ src/pages/Classroom/BookedCourse.js
@@ -6,7 +6,14 @@ const BookedCourse = () => {
   const courses = [];
 
   if (courses.length === 0) {
-    return <NoCourese content={{ name: "Booked", description: "fjkfd" }} />;
+    return (
+      <NoCourese
+        content={{
+          name: "Booked",
+          description: "You Don't Have Any course Booked",
+        }}
+      />
+    );
   }
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-[30px]">
--- src/pages/Classroom/OnGoingCourse.js
+++ src/pages/Classroom/OnGoingCourse.js
@@ -3,10 +3,17 @@ import ClassroomCard from "./ClassroomCard";
 import NoCourese from "./NoCourese";
 
 const OnGoingCourse = () => {
-  const courses = [0, 1];
+  const courses = [];
 
   if (courses.length === 0) {
-    return <NoCourese content={{ name: "OnGoing", description: "fjkfd" }} />;
+    return (
+      <NoCourese
+        content={{
+          name: "OnGoing",
+          description: "You Don't Have Any on-going courses",
+        }}
+      />
+    );
   }
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-[30px]">
